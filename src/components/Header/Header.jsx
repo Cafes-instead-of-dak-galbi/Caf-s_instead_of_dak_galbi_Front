464b4a36a2eef2ea08265860a329e67d261e1720
Kyu-ddung <gyuwann@gmail.com>
2025-10-13 16:18:32 +0900

    feat: 페이지 헤더 컴포넌트 완성

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 import "../../styles/components/Header.css";
 
 const Header = () => {
   return (
-    <div>
-        페이지 최상단에 들어가는 공통 헤더 컴포넌트 페이지 입니다.
-    </div>
-  )
-}
+    <header className="header_containor">
+      <nav className="nav">
+        <span className="page_name">☕ Chuncheon Café</span>
+        <ul className="nav_list">
+          <li><Link to="/popular">인기</Link></li>
+          <li><Link to="/map">지도</Link></li>
+          <li><Link to="/events">행사</Link></li>
+          <li><Link to="/stories">스토리</Link></li>
+        </ul>
+      </nav>
+      <div className="header_right">
+        <Link to="/cafes/new" className="create_cafe">카페 등록</Link>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "../../styles/pages/Home.css";
-
+import Header from "../../components/Header/Header";
 const Home = () => {
   return (
-    <div>처음에 들어오면 로딩 되는 메인 페이지 입니다.</div>
+    <div>
+      <Header/>
+      처음에 들어오면 로딩 되는 메인 페이지 입니다.</div>
   )
 }
 
