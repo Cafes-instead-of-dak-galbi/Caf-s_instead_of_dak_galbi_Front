a157b434181d77484018790343d0a4c5834fec84
unknown <leb0330@naver.com>
2025-10-21 17:52:27 +0900

    작업한 내용 저장

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React from "react";
 import "../../styles/pages/Home.css";
-import Header from "../../components/Header/Header";
-const Home = () => {
-  return (
-    <div>
-      <Header/>
-      처음에 들어오면 로딩 되는 메인 페이지 입니다.</div>
-  )
-}
 
-export default Home;
+const CafeMain = () => {
+  return(
+<div className="split">{/* 좌우 2분할 레이아웃 */}
+      {/* 왼쪽: 카페 리스트 영역 */}
+      <aside className="sidebar" aria-label="카페 리스트">
+        <header className="brand">
+          <h1 className="title">닭갈비 말고 카페</h1>
+          {/* <p className="subtitle">춘천 감성 카페 모음</p> */}
+        </header>
+
+         <section className="list">
+          <div className="placeholder">
+            여기에 카페 리스트가 들어갑니다.
+            {/* 추후 지도 연결*/}
+          </div>
+        </section>
+      </aside>
+      <main className="content" aria-label="지도">
+        <section className="map-wrap">
+          <div id="mapPlaceholder" className="map-placeholder">
+            지도 영역 (카카오맵 연동)
+          </div>
+        </section>
+      </main>
+
+
+      </div>
+
+      //지도 영역
+
+  );
+};
+
+export default CafeMain;
